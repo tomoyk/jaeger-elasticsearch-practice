e84e2ac358d25f1ba56521e50e3daa5afe4373b4
tomoyk <tmyk.kym@gmail.com>
2024-12-23 02:59:49 +0000

    fixed

--- chaos-mesh/chaos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CURRENT=$(date '+%Y%m%d-%H%M%S')
-AFTER_10M=$(date '+%Y%m%d-%H%M%S' -d "10 minute")
-echo "Chaos start ${CURRENT}"
-sed "s/test30/test${CURRENT}/g" job.yaml | kubectl apply -f -
-echo "Chaos end ${AFTER_10M}"
--- chaos-mesh/get-blackbox-logs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-kubectl logs -l name=front-end -c istio-proxy -n sock-shop -f
-# kubectl logs -l name=catalogue -c istio-proxy -n sock-shop #| grep Blackbox
